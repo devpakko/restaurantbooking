3a3a6dc85cfc00136a9e2826178b8ce5c2bbda15
devpako <developerpako@gmail.com>
2023-11-23 19:50:32 +0400

    refactor code into client and server-side components and create layout

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,20 +1,15 @@
-import Link from "next/link"
-import NavBar from "../../components/NavBar"
 import Header from "./components/Header"
 import Form from "./components/Form"
 
 export default function Reserve(){
     return (
-        <main className="bg-gray-100 min-h-screen">
-          <div className="max-w-screen-2xl m-auto bg-white">
-            <NavBar/>
-            <div className="border-t h-screen flex justify-center">
-              <div className="py-9 m-auto">
-                <Header/>
-                <Form/>
-              </div>
+        <>
+          <div className="border-t h-screen flex justify-center">
+            <div className="py-9 m-auto">
+              <Header/>
+              <Form/>
             </div>
           </div>
-        </main>
+        </>
     )
 }
